183d6e345cc9157dcf256034386b0831d40bd693
HailMuffins <rafaelsm123@outlook.com>
2022-07-28 18:43:10 -0300

    Requisito 3 Finalizado, Lint Apaziguado

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+
+  return employees.find(
+    (elem) => elem.firstName === employeeName || elem.lastName === employeeName,
+  );
 }
 
 module.exports = getEmployeeByName;
