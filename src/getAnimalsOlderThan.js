74537304fd35553806cc56585eb12c3e9ae11a25
HailMuffins <rafaelsm123@outlook.com>
2022-07-28 18:31:16 -0300

    Requisito 2 Feito, Lint Apaziguado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  return species.find((elem) => elem.name === animal).residents.every((elem) => elem.age >= age);
 }
+
+console.log(getAnimalsOlderThan('lions', 20));
 
 module.exports = getAnimalsOlderThan;
