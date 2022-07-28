bc859bef8a5daf6b2049057af5526503cdd3339c
HailMuffins <rafaelsm123@outlook.com>
2022-07-28 17:57:26 -0300

    Requisito 1 Feito, Lint Incompleto

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,21 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  if (ids.length === 0) {
+    return [];
+  }
+
+  if (ids.length === 1) {
+    return [species.find((elem) => elem.id === ids[0])];
+  }
+  const speciesReturn = species.filter((elem) => {
+    if (ids.includes(elem.id)) {
+      return elem.id;
+    }
+  });
+  return speciesReturn;
 }
 
 module.exports = getSpeciesByIds;
