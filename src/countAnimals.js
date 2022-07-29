ee85c7e461341177d030bf44c78fc03a2b5c7950
HailMuffins <rafaelsm123@outlook.com>
2022-07-29 19:19:50 -0300

    Requisito 5 Finalizado, Lint Apaziguado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return data.species.reduce(
+      (a, e) => ({ ...a, [e.name]: e.residents.length }),
+      {},
+    );
+  }
+  if ('specie' in animal && 'sex' in animal) {
+    return data.species
+      .find((e) => e.name === animal.specie)
+      .residents.filter((e) => e.sex === animal.sex).length;
+  } if ('specie' in animal) {
+    return data.species.find((e) => e.name === animal.specie).residents.length;
+  }
 }
 
+console.log(countAnimals({ specie: 'lions' }));
 module.exports = countAnimals;
