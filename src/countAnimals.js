9d039208f6d10d646480dd9e4a1677bfecb80565
HailMuffins <rafaelsm123@outlook.com>
2022-07-29 19:22:20 -0300

    Requisito 5 Refatorado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -7,13 +7,12 @@ function countAnimals(animal) {
       {},
     );
   }
-  if ('specie' in animal && 'sex' in animal) {
-    return data.species
+
+  return 'specie' in animal && 'sex' in animal
+    ? data.species
       .find((e) => e.name === animal.specie)
-      .residents.filter((e) => e.sex === animal.sex).length;
-  } if ('specie' in animal) {
-    return data.species.find((e) => e.name === animal.specie).residents.length;
-  }
+      .residents.filter((e) => e.sex === animal.sex).length
+    : data.species.find((e) => e.name === animal.specie).residents.length;
 }
 
 console.log(countAnimals({ specie: 'lions' }));
