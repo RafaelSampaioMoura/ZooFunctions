bbc24f378376eb8e939ea7a560808ba0fc1dad7b
HailMuffins <rafaelsm123@outlook.com>
2022-07-30 11:13:29 -0300

    Resetando repositório

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,8 @@
+const { default: test, it } = require('test');
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+    test('Nenhum parâmetro deve retornar undefined', () => {
+        expect(handlerElephants()).toBeUndefined();
+    })
+});
