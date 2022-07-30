f4c22e3f223ebd3d0e9442c36aacbd30c3112e4a
HailMuffins <rafaelsm123@outlook.com>
2022-07-30 11:34:18 -0300

    Requisito 8, Lint Apaziguado

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,45 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  const ageRange = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((e) => {
+    if (e.age < 18) {
+      ageRange.child += 1;
+    } else if (e.age < 50) {
+      ageRange.adult += 1;
+    } else {
+      ageRange.senior += 1;
+    }
+  });
+  return ageRange;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (!entrants) {
+    return 0;
+  }
+
+  if (JSON.stringify(entrants) === '{}') {
+    return 0;
+  }
+  const people = countEntrants(entrants);
+  return (
+    people.child * data.prices.child
+    + people.adult * data.prices.adult
+    + people.senior * data.prices.senior
+  );
 }
+
+console.log(
+  calculateEntry([
+    { name: 'Lara Carvalho', age: 5 },
+    { name: 'Frederico Moreira', age: 5 },
+    { name: 'Pedro Henrique Carvalho', age: 5 },
+    { name: 'Maria Costa', age: 18 },
+    { name: 'Núbia Souza', age: 18 },
+    { name: 'Carlos Nogueira', age: 50 },
+  ]),
+);
 
 module.exports = { calculateEntry, countEntrants };
