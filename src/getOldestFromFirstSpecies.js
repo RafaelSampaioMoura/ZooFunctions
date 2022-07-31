f04b562bcd89492c40256ba79a362f8c5a8feb40
HailMuffins <rafaelsm123@outlook.com>
2022-07-31 15:49:58 -0300

    Requisito 10 Finalizado, Lint Apaziguado

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,6 +2,13 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const oldestAnimal = data.species
+    .find(
+      (e) => e.id === data.employees.find((i) => i.id === id).responsibleFor[0],
+    )
+    .residents.reduce((a, o) => (o.age < a.age ? a : o));
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 }
 
+console.log(getOldestFromFirstSpecies('c1f50212-35a6-4ecd-8223-f835538526c2'));
 module.exports = getOldestFromFirstSpecies;
