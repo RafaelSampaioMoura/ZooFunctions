c18b75b324060e0d6d882495da9847594b052332
HailMuffins <rafaelsm123@outlook.com>
2022-08-08 18:44:59 -0300

    .

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,69 +1,4 @@
-// const { default: test } = require('test');
-// const { default: test } = require('test');
-// const { default: test } = require('test');
 const { handlerElephants } = require('../src/handlerElephants');
-
-// const elephantsId = 'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5';
-
-// const theElephants = {
-//   id: elephantsId,
-//   name: 'elephants',
-//   popularity: 5,
-//   location: 'NW',
-//   availability: ['Friday', 'Saturday', 'Sunday', 'Tuesday'],
-//   residents: [
-//     {
-//       name: 'Ilana',
-//       sex: 'female',
-//       age: 11,
-//     },
-//     {
-//       name: 'Orval',
-//       sex: 'male',
-//       age: 15,
-//     },
-//     {
-//       name: 'Bea',
-//       sex: 'female',
-//       age: 12,
-//     },
-//     {
-//       name: 'Jefferson',
-//       sex: 'male',
-//       age: 4,
-//     },
-//   ],
-// };
-// describe('Testes das funções auxiliares de HandlerElephants', () => {
-//   test('"getElephants retorna a espécie "elephants', () => {
-//     expect(getElephants()).toEqual(theElephants);
-//   });
-
-//   test('averageAge retorna a média correta das idades dos elefantes', () => {
-//     expect(averageAge({ residents: theElephants.residents })).toBe(10.5);
-//   });
-
-//   test('computeData retorna a quantidade correta dos elefantes no zoológico', () => {
-//     expect(computeData('count', theElephants)).toBe(4);
-//   });
-
-//   test('computeData retorna um array com o nomes dos elefantes', () => {
-//     expect(computeData('names', theElephants)).toEqual([
-//       'Ilana',
-//       'Orval',
-//       'Bea',
-//       'Jefferson',
-//     ]);
-//   });
-
-//   test('computeData retorna a média correta das idades dos elefantes', () => {
-//     expect(computeData('averageAge', theElephants)).toBe(10.5);
-//   });
-
-//   test('computeData retorna nulo quando não há parâmetros corretos', () => {
-//     expect(computeData('bullshit', theElephants)).toBeNull();
-//   });
-// });
 
 describe('Testa a função handlerElephants principal', () => {
   test('Sem handlerElephants não tiver um parâmetro, retorna undefined', () => {
@@ -72,15 +7,9 @@ describe('Testa a função handlerElephants principal', () => {
 
   test('Retorna mensagem de erro quando o parâmetro não é uma string', () => {
     const noString = 'Parâmetro inválido, é necessário uma string';
-    expect(handlerElephants(1224345)).toBe(
-      noString,
-    );
-    expect(handlerElephants(['abloogie', 'woogie', 'woo'])).toBe(
-      noString,
-    );
-    expect(handlerElephants({ abloogie: 'woogie' })).toBe(
-      noString,
-    );
+    expect(handlerElephants(1224345)).toBe(noString);
+    expect(handlerElephants(['abloogie', 'woogie', 'woo'])).toBe(noString);
+    expect(handlerElephants({ abloogie: 'woogie' })).toBe(noString);
   });
 
   test('Retorna a informação correta', () => {
